d705bce0d96f71177d50358d18b806871f1e49a4
DeepFusion <dev@deepfusion.local>
2026-06-01 15:33:00 +0800

    cleanup

--- _check_caixin.py
+++
@@ -1,6 +0,0 @@
-"""Check Caixin data functions."""
-import akshare as ak
-funcs = [x for x in dir(ak) if 'caixin' in x.lower() or 'cx_' in x.lower() or '财新' in x]
-for f in sorted(funcs):
-    doc = getattr(ak, f).__doc__ or ''
-    print(f'{f:50s} {doc[:60]}')
